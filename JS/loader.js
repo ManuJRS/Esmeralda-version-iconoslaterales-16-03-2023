722ee3b917d58d47aa15aa0141c279f0795c55fb
ManuR <ma_nuel_rs@hotmail.com>
2023-03-24 09:48:53 -0600

    nueva version con alerta simple

--- JS/loader.js
+++ JS/loader.js
@@ -1,6 +1,8 @@
-window.onload = function(){
-
+window.onload = function() {
     $('#onload').fadeOut();
     $('body').removeClass('hidden');
-
-}
+    
+    setTimeout(function() {
+      alert("¡Hey!, debes ser MAYOR de edad para entrar a la página 😎🍻");
+    }, 1000);
+  };
